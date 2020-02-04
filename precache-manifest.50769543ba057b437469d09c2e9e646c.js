1d91a7a40ca832e9b881b407dbdc792da7b1e7e0
dkiselev <dmitry.v.kiselev@gmail.com>
2020-02-03 21:29:19 -0400

    Updates

---
+++ precache-manifest.50769543ba057b437469d09c2e9e646c.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f123d3514fc09a5bf38f4d9181bb6e41",
+    "url": "/piano-reader/index.html"
+  },
+  {
+    "revision": "ada59b3b9f6ebf33b717",
+    "url": "/piano-reader/static/css/2.1e78366a.chunk.css"
+  },
+  {
+    "revision": "8f95410e1a4b5e9c7d89",
+    "url": "/piano-reader/static/css/main.af727b1e.chunk.css"
+  },
+  {
+    "revision": "ada59b3b9f6ebf33b717",
+    "url": "/piano-reader/static/js/2.b641ba6a.chunk.js"
+  },
+  {
+    "revision": "4d5bf68e0c7b0ab80a575268b64665d4",
+    "url": "/piano-reader/static/js/2.b641ba6a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "8f95410e1a4b5e9c7d89",
+    "url": "/piano-reader/static/js/main.16a15f6d.chunk.js"
+  },
+  {
+    "revision": "c87c107ef52d288d2b44",
+    "url": "/piano-reader/static/js/runtime-main.4158f268.js"
+  }
+]);
